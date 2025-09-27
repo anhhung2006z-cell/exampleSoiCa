b3b98d992611e77cea1f243580591aeebc7e94e5
Nguyễn Anh Hùng <anhhung2006z@gmail.com>
2025-09-27 21:08:59 +0700

    FISRT COMIT

---
+++ bài 3/bài 3/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Gán sẵn danh sách tên sinh viên
+        List<string> sinhVien = new List<string>
+        {
+            "An",
+            "Nguyen Van Minh",
+            "Ngo Quoc Minh",
+            "Nguyen Ngoc Nam",
+            "Le Ngoc Bao"
+        };
+
+        Console.WriteLine("===== DANH SÁCH SINH VIÊN =====");
+        foreach (string ten in sinhVien)
+        {
+            Console.WriteLine(ten);
+        }
+
+        // tìm tên dài nhất
+        string tenDaiNhat = sinhVien[3];
+        foreach (string ten in sinhVien)
+        {
+            if (ten.Length > tenDaiNhat.Length)
+            {
+                tenDaiNhat = ten;
+            }
+        }
+
+        Console.WriteLine($"\nTên sinh viên dài nhất: {tenDaiNhat} ({tenDaiNhat.Length} ký tự)");
+    }
+}
