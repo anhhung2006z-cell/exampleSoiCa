458eb69d31bb8157be654f378efa59b2d697f1f9
Nguyễn Anh Hùng <anhhung2006z@gmail.com>
2025-10-11 21:48:31 +0700

    FIRST COMMIT

---
+++ Danh sách đt/Danh sách đt/Program.cs
@@ -0,0 +1,42 @@
+﻿class Phone
+{
+    public string Name { get; set; }
+    public int Price { get; set; }
+
+    public Phone(string name, int price)
+    {
+        Name = name;
+        Price = price;
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Name: {Name}, Price: {Price}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+ 
+        List<Phone> phones = new List<Phone>()
+        {
+            new Phone("iPhone", 20000),
+            new Phone("Samsung", 24000),
+            new Phone("Xiaomi", 14000)
+        };
+
+        Console.WriteLine("Danh sách điện thoại ban đầu:");
+        foreach (var p in phones)
+            p.ShowInfo();
+
+        Phone p1 = phones[0];
+        p1.Price = 9999;
+
+        Console.WriteLine("\nDanh sách điện thoại sau khi thay đổi giá p1:");
+        foreach (var p in phones)
+            p.ShowInfo();
+
+    }
+}
